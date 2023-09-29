e99fe56bd02452a7b1680593f1408c47cff5eb35
JuYeung2 <bcd328@naver.com>
2023-09-30 05:25:47 +0900

    데이터 SQL 저장

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import { connection } from './connection';
+import userRouter from './router/user'; // user라우터
+import cors from 'cors';
 
 const app: Express = express();
 const port = 8080;
@@ -8,15 +10,23 @@ const port = 8080;
 const connectDB = async () =>{
   try {
     await connection.connect();
-    console.log("연결되었습니다");
+    console.log("DB연결");
   }catch (err){
     console.error("실패",err);
   }
 };
 
-// 시작시 연결 시도
+// 서버 시작시 연결
 connectDB();
+
+// 미들웨어 설정
+app.use(express.json());
+app.use(cors());
+
+// 라우터 사용
+app.use('/user',userRouter); // /user 경로로 요청이 들어오면 실행
 
 app.listen(port,()=>{
   console.log(`Server on ${port}`);
 });
+
